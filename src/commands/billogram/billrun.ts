9456d169af4161efdb4e0c6c82307c2ee6147687
weactivist <rasmus@billogram.com>
2021-03-29 14:24:33 +0200

    Improved chunk-size flag and clarified command descriptions

--- src/commands/billogram/billrun.ts
+++ src/commands/billogram/billrun.ts
@@ -2,15 +2,15 @@ import {Command, flags} from '@oclif/command'
 const utils = require('../../utils')
 
 export default class Billrun extends Command {
-  static description = 'build oclif'
+  static description = 'Simulate a billrun. A new recipient will be created that is assigned all the invoices created.'
 
   static examples = [
     '$ billoapi billogram:billrun',
   ]
 
   static flags = {
-    limit: flags.string({default: '10'}),
-    chunk: flags.string({default: '100'}),
+    limit: flags.string({default: '10', description: 'Limit the number of invoices created.'}),
+    'chunk-size': flags.string({default: '10', description: 'This splits the number of invoices into chunks where each chunk has its own recipient. Example: A limit of 10 invoices with a chunk size of 5 would create two new recipients having 5 invoices each.'}),
     help: flags.help({char: 'h'}),
   }
 
@@ -21,17 +21,17 @@ export default class Billrun extends Command {
 
     const config = await utils.credentials(this)
 
-    const chunk = parseInt(flags.chunk, 10)
+    const chunkSize = parseInt(flags['chunk-size'], 10)
 
-    for (let i = 0, j = parseInt(flags.limit, 10); i < j; i += chunk) {
+    for (let i = 0, j = parseInt(flags.limit, 10); i < j; i += chunkSize) {
       /* eslint-disable no-await-in-loop */
       const {body} = await utils.create_customer(config)
       const customerNumber = body.data.customer_no
       this.log('Created customer with customer number:', body.data.customer_no)
 
-      const promises = [...new Array(chunk).keys()].map(async () => {
+      const promises = [...new Array(chunkSize).keys()].map(async () => {
         const {body} = await utils.create_billogram(config, customerNumber)
-        this.log('Created invoice with ID:', body.data.id)
+        // this.log('Created invoice with ID:', body.data.id)
       })
 
       await Promise.all(promises)
